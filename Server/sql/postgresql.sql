7ee3e37a36c598eebb74cd7ca91ce927ac588eac
Lor Hengrith <155905861+SthNoWork@users.noreply.github.com>
2026-02-03 16:33:11 +0700

    feat: Add configuration section for key management and enhance record encryption

--- Server/sql/postgresql.sql
+++ Server/sql/postgresql.sql
@@ -37,3 +37,6 @@ GRANT ALL ON patient_records TO anon;
 GRANT ALL ON users TO anon;
 GRANT USAGE, SELECT ON SEQUENCE patient_records_id_seq TO anon;
 GRANT USAGE, SELECT ON SEQUENCE users_id_seq TO anon;
+
+INSERT INTO users (username, password_hash, role) VALUES ('doctor_bob', '5a6cc72b29128f7b42d44158bb89f2de3478e5dfa41903dc57ec27ebe0ffef41', 'doctor');
+INSERT INTO users (username, password_hash, role) VALUES ('nurse_akino', '5a6cc72b29128f7b42d44158bb89f2de3478e5dfa41903dc57ec27ebe0ffef41', 'nurse');
